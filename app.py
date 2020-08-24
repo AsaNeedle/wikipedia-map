3faf7941146cefd6d2d31216d8349683e08ed410
AsaNeedle <asa@cottageclass.com>
2020-08-24 15:28:46 -0400

    Revert "requests go to main page"
    This reverts commit f6827075310be0a8a5fd9c5489e41e8eb3852df9.

--- app.py
+++ app.py
@@ -6,21 +6,24 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    if request.method == 'POST':
-        thinker = request.form.get('thinker')
-        try:
-            thinker_underscore, title = create_graph(thinker)
-            with open("static/img/%s.svg" % thinker_underscore) as svg:
-                return render_template('main.html', thinker=title, svg=svg.read())
-        except wikipedia.exceptions.DisambiguationError as e:
-            return render_template('main.html', disambiguation_options=e.options)
-        except Exception as e:
-            return render_template('main.html', error="Sorry 🤷‍♂️, I don't know them. Try someone else!")
-    else:
-        return render_template("main.html", result=None)
+    return render_template("main.html")
 @app.route('/about')
 def about():
     return render_template("about.html")
+@app.route('/thinker/<name>')
+def hello_world(name=None):
+    return render_template("main.html")
+@app.route("/action", methods = ['POST'])
+def action():
+    thinker = request.form.get('thinker')
+    try:
+        thinker_underscore, title = create_graph(thinker)
+        with open("static/img/%s.svg" % thinker_underscore) as svg:
+            return render_template('action.html', thinker=title, svg=svg.read())
+    except wikipedia.exceptions.DisambiguationError as e:
+        return render_template('main.html', disambiguation_options=e.options)
+    except Exception as e:
+        return render_template('main.html', error="Sorry 🤷‍♂️, I don't know them. Try someone else!")
 
 if __name__ == '__main__':
     app.run()
