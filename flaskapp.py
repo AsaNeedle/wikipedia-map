fce6738c83ddba1ef85daa97eddeebd829c1efa4
Asa Needle <asa@asaneedle.com>
2021-03-31 10:40:30 -0400

    update router

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from create_graph import create_graph
+from create_graph import create_graph, get_beautiful_soup
 import wikipedia
 import sys
 app = Flask(__name__)
@@ -17,12 +17,18 @@ def hello_world(name=None):
 def action():
     thinker = request.form.get('thinker')
     try:
-        thinker_underscore, title = create_graph(thinker)
-        with open("static/img/%s.svg" % thinker_underscore) as svg:
-            return render_template('action.html', thinker=title, svg=svg.read())
-    except wikipedia.exceptions.DisambiguationError as e:
-        return render_template('main.html', disambiguation_options=e.options)
+        response = get_beautiful_soup(thinker)
+        # If Wikipedia raises DisambiguationError, shows you the options
+        if isinstance(response, list):
+            return render_template('main.html', disambiguation_options=response.options)
+        else: 
+            title, graph = create_graph(response)
+            link_name = title.replace(" ", "_")
+            graph.write_svg(f"./static/img/{link_name}.svg")
+            with open(f"./static/img/{link_name}.svg") as svg:
+                return render_template('action.html', thinker=title, svg=svg.read())
     except Exception as e:
+        print(e)
         return render_template('main.html', error="Sorry 🤷‍♂️, I don't know them. Try someone else!")
 
 if __name__ == '__main__':
